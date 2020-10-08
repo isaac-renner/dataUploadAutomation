1bf66de9b2ea962ef550dc5a64f23155ee815fb6
Isaac Renner <isaac@trunkplatform.com>
2020-10-08 20:09:36 +1100

    Completed Metabase Sync

--- metabaseSync.py
+++ metabaseSync.py
@@ -1,8 +1,10 @@
 import requests
 import csv 
+import json
+
 import os
 from dotenv import load_dotenv
-project_folder = os.path.expanduser('./my-project-dir') 
+project_folder = os.path.expanduser('.') 
 load_dotenv(os.path.join(project_folder, '.env'))
 
 headingToken = os.getenv("SESSIONID")
@@ -21,12 +23,14 @@ questions_to_export = [
     72
 ]   
 
-url = 'https://metabase.ailo.io/api/card/65/query/csv' 
-headers = {'X-Metabase-Session':SheadingToken} 
-    
-r = requests.post(url, headers=headers)
 
-f = open('metabase/autoWithdraw.csv', 'w')
-f.write(r.text)
-f.close
+for i in range( len(questions_to_export)): 
+    url = 'https://metabase.ailo.io/api/card/' + str(questions_to_export[i]) 
+    headers = {'X-Metabase-Session':headingToken} 
+    name_request = requests.get(url, headers=headers)
+    csv_request = requests.post(url+'/query/csv', headers=headers)
+    f = open('metabase/'+json.loads(name_request.text)["name"]+'.csv', "w")
+    f.write(csv_request.text)
+    f.close
+
 print('done')
