ba0db8e0ab067d75608354618860904ac093417b
Isaac Renner <isaac@trunkplatform.com>
2020-10-09 11:09:42 +1100

    Create date stamped file when importing metabase data

--- src/metabaseSync.py
+++ src/metabaseSync.py
@@ -1,6 +1,8 @@
 import requests
 import csv 
 import json
+import datetime
+import time
 
 import os
 from dotenv import load_dotenv
@@ -8,7 +10,6 @@ project_folder = os.path.expanduser('.')
 load_dotenv(os.path.join(project_folder, '.env'))
 
 headingToken = os.getenv("SESSIONID")
-print(headingToken)
 
 questions_to_export = [
     56, 
@@ -23,13 +24,24 @@ questions_to_export = [
     72
 ]   
 
+path = 'metabase/' +  datetime.date.today().strftime("%d-%m-%y") + '/' 
 
+def mkdir(): 
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print("Directory " , path,  " Created ")
+    else:
+        print("Directory " , path,  " already exists")
+
+
+mkdir()
 for i in range( len(questions_to_export)): 
     url = 'https://metabase.ailo.io/api/card/' + str(questions_to_export[i]) 
     headers = {'X-Metabase-Session':headingToken} 
-
+    name_request = requests.get(url, headers=headers)
     csv_request = requests.post(url+'/query/csv', headers=headers)
-    f = open('metabase/'+json.loads(name_request.text)["name"]+'.csv', "w")
+    print(csv_request.text)
+    f = open(path + json.loads(name_request.text)["name"]+'.csv', "x")
     f.write(csv_request.text)
     f.close
 
