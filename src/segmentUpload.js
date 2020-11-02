24802b56c59e8b97d70a68614e9c004fa7c9838f
Isaac Renner <isaac@trunkplatform.com>
2020-11-03 10:11:42 +1100

    Quick refacor + added segment upload automation

---
+++ src/segmentUpload.js
@@ -0,0 +1,141 @@
+// dependencies
+var async = require('async');
+const fs = require('fs');
+const path = require('path');
+
+// csvtojson quickly and easily parses and formats our CSV files
+var csv = require('csvtojson');
+// the following are Segment libraries
+var Analytics = require('analytics-node');
+var Objects = require('objects-node');
+
+const dotenv = require('dotenv');
+dotenv.config();
+var analytics = new Analytics(process.env.write_key);
+var objects = new Objects(process.env.write_key);
+
+
+
+const sendToSegment = function(callback, srcFileName) {
+    // Read options from the event.
+    //console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+    // File name may have spaces or unicode non-ASCII characters.
+    // Download the CSV from S3, transform, and upload to Segment.
+    // More on async.waterfall here: https://caolan.github.io/async/docs.html#waterfall
+
+    async.waterfall([
+        function download(next) {
+            // Download the CSV from S3 into a buffer.
+            const data = fs.readFileSync(path.resolve(__dirname, '../sheets/02-11-20/'+srcFileName))
+            next(null,data.toString())
+        },
+        function transform(csvString , next) {
+            console.log("transform");
+            //console.log(csvString)
+            // In colParser we ensure that our timestamps aren't strings, Segment APIs don't like strings here
+            csv({
+            	colParser:{
+            		"createdAt":function(item){return new Date(item);},
+                "timestamp":function(item){return new Date(item);},
+            	}
+            })
+            .fromString(csvString)
+            .then((formattedResults)=>{
+              next(null,formattedResults);
+            })
+        },
+        function upload(formattedResults, next) {
+            //console.log("upload");
+            if(srcFileName.includes('identify')){
+              formattedResults.map(function(identifyObject){
+                // More in the docs here: https://segment.com/docs/connections/spec/identify/
+                analytics.identify(identifyObject)
+              });
+            }else if(srcFileName.includes('track')){
+              formattedResults.map(function(trackObject){
+                // More in the docs here: https://segment.com/docs/connections/spec/track/
+                analytics.track(trackObject);
+              });
+            }else if(srcFileName.includes('page')){
+              formattedResults.map(function(pageObject){
+                // More in the docs here: https://segment.com/docs/connections/spec/page/
+                analytics.page(pageObject);
+              });
+            }else if(srcFileName.includes('screen')){
+              formattedResults.map(function(screenObject){
+                // More in the docs here: https://segment.com/docs/connections/spec/screen/
+                analytics.screen(screenObject);
+              });
+            }else if(srcFileName.includes('group')){
+              formattedResults.map(function(groupObject){
+                // More in the docs here: https://segment.com/docs/connections/spec/group/
+                analytics.group(groupObject);
+              });
+            }else if(srcFileName.includes('alias')){
+              formattedResults.map(function(aliasObject){
+                // More in the docs here: https://segment.com/docs/connections/spec/alias/
+                analytics.alias(aliasObject);
+              });
+            }else if(srcFileName.includes('object')){
+              // Who doesn't love a variable named objectObject?! :(
+              formattedResults.map(function(objectObject){
+                // The Object API accepts a different format than the other APIs
+                // More in the docs here: https://github.com/segmentio/objects-node
+                // First, we get our collection name
+                var objectCollection = srcFileName.split("_")[1];
+                // Then, we get and delete our objectId from the object we pass into Segment
+                var objectId = objectObject.id;
+                delete objectObject.id;
+                console.log("objectCollection: ",objectCollection);
+                console.log("objectId: ", objectId);
+                console.log("objectObject: ",objectObject);
+                objects.set(objectCollection, objectId, objectObject);
+              });
+            }else{
+              console.log("ERROR! No call type specified! Your CSV file in S3 should start with 'identify_', 'track_', 'page_', 'screen_', 'group_', 'alias_' or 'object_<collection>'");
+              throw new Error;
+            }
+            // Now we make sure that all of our queued actions are flushed before moving on
+            if(srcFileName.startsWith('object_')){
+              var objectCollection = srcFileName.split("_")[1];
+              objects.flush(objectCollection, function(err, batch){
+                next(err,"Done");
+              });
+            }else{
+              analytics.flush(function(err, batch){
+                console.log(srcFileName + ' Flushed, and now this program can exit!');
+            });
+            }
+        }
+      ], function (err) {
+            // Some pretty basic error handling
+            if (err) {
+                console.error(
+                    'Unable to download ' + srcBucket + '/' + srcFileName +
+                    ' and upload to Segment' +
+                    ' due to an error: ' + err
+                );
+            } else {
+                console.log(
+                    'Successfully downloaded ' + srcBucket + '/' + srcFileName +
+                    ' and uploaded to Segment!'
+                );
+            }
+            callback(null, "Success!");
+        }
+    );
+};
+
+var files = fs.readdirSync('/Users/isaacrenner/Documents/automation/autoUpload/sheets/02-11-20');
+files.forEach(file => 
+    {
+        if(file.includes('Segment')){
+            sendToSegment((data)=> (data) , file);
+        }
+    }
+)
+
+
+
+
+
--- src/uploadDataToSegement.py
+++ src/uploadDataToSegement.py
@@ -1,5 +1,50 @@
 import requests
 import csv 
+import json 
 
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('.') 
+load_dotenv(os.path.join(project_folder, '.env'))
+
+auth = os.getenv("SEGMENT_AUTH")
+
+
+data = {
+    "action": "identify",
+    "userId": "ailo:authz:user:2991",
+    "traits.created": "2020-06-25T05:25:37.111Z",
+    "traits.firstName": "Sherry",
+    "traits.lastName": "Investor",
+    "traits.email": "sherry+investor@ailo.io",
+    "traits.management_id": [
+      "212d90d9-3737-4611-bcc2-aeb13f3401ce"
+    ],
+    "traits.first_payment_method_added": "2020-08-15T00:55:51.038346Z"
+  }
+  
+def main(): 
+    sync_id = open()
+    close(sync_id) 
+
+def open(): 
+    url = "https://objects-bulk-api.segmentapis.com/v0/start"
+    headers = {'Authorization':auth} 
+    request = requests.post(url, headers=headers)
+    return json.loads(request.content)['sync_id']
+
+
+def upload(id): 
+    url =  "https://objects-bulk-api.segmentapis.com/v0/upload/" + id
+    headers = {'Authorization':auth} 
+
+
+
+def close(id): 
+    url = "https://objects-bulk-api.segmentapis.com/v0/finish/" + id 
+    print(url)
+    headers = {'Authorization':auth,'Content-Type':'application/json'} 
+    request = requests.post(url, headers=headers)
+    print(request.content)
 
 
